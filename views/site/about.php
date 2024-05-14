f73e3a264e25114a7f5005c6ea06d907bb971917
4eshir <g.kalashnik@mail.ru>
2024-05-14 10:03:01 +0400

    three js

--- views/site/about.php
+++ views/site/about.php
@@ -7,12 +7,29 @@ use yii\helpers\Html;
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+
+<style>
+    body { margin: 0; }
+    canvas { width: 100%; height: 100% }
+</style>
+
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
+    <!-- <p>
         This is the About page. You may modify the following file to customize its content:
     </p>
 
-    <code><?= __FILE__ ?></code>
+    <code><?= __FILE__ ?></code> -->
 </div>
+
+<script type="module">
+    import  * as THREE from '/three';
+    /*import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+    import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+
+
+    const controls = new OrbitControls( camera, renderer.domElement );
+    const loader = new GLTFLoader();*/
+</script>

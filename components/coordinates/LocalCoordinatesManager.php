427aff7e5ddac9e7b17cb10c69424e3b64d88145
4eshir <g.kalashnik@mail.ru>
2024-06-12 16:00:28 +0400

    data fixes

--- components/coordinates/LocalCoordinatesManager.php
+++ components/coordinates/LocalCoordinatesManager.php
@@ -18,12 +18,12 @@ class LocalCoordinatesManager
         $centerObject = [
             'x' =>
                 $objectExt->positionType == TerritoryConcept::HORIZONTAL_POSITION ?
-                    $objectExt->left + ObjectWork::convertDistanceToCells($objectExt->object->length / 2, TerritoryConcept::STEP) :
+                    $objectExt->left - ObjectWork::convertDistanceToCells($objectExt->object->length / 2, TerritoryConcept::STEP) :
                     $objectExt->top + ObjectWork::convertDistanceToCells($objectExt->object->width / 2, TerritoryConcept::STEP),
             'y' =>
                 $objectExt->positionType == TerritoryConcept::HORIZONTAL_POSITION ?
                     $objectExt->top + ObjectWork::convertDistanceToCells($objectExt->object->width / 2, TerritoryConcept::STEP) :
-                    $objectExt->left + ObjectWork::convertDistanceToCells($objectExt->object->length / 2, TerritoryConcept::STEP),
+                    $objectExt->left - ObjectWork::convertDistanceToCells($objectExt->object->length / 2, TerritoryConcept::STEP),
         ];
 
         $centerTerritory = [

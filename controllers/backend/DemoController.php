85e63e12264a5666beaae2ba074d811345e8213f
4eshir <g.kalashnik@mail.ru>
2024-06-12 13:08:50 +0400

    data fixes

--- controllers/backend/DemoController.php
+++ controllers/backend/DemoController.php
@@ -5,6 +5,7 @@ namespace app\controllers\backend;
 use app\components\arrangement\TemplatesManager;
 use app\components\arrangement\TerritoryConcept;
 use app\facades\TerritoryFacade;
+use app\models\ObjectExtended;
 use app\models\work\TerritoryWork;
 use app\models\search\SearchTerritoryWork;
 use yii\web\Controller;
@@ -29,6 +30,43 @@ class DemoController extends Controller
     public function actionGenerateTemplateAjax($tId)
     {
         $this->facade->generateTerritoryArrangement(TerritoryConcept::TYPE_BASE_WEIGHTS, 1, TerritoryFacade::OPTIONS_DEFAULT, $tId);
-        return json_encode(['result' => $this->facade->getRawMatrix()]);
+        $matrix = $this->facade->getRawMatrix();
+        $objectsList = $this->facade->model->objectsPosition;
+        $resultObjList = [];
+        $maxHeight = 0;
+
+        foreach ($objectsList as $objectExt) {
+            /** @var ObjectExtended $objectExt */
+            if ($maxHeight < $objectExt->object->height) {
+                $maxHeight = $objectExt->object->height;
+            }
+
+            $resultObjList[] = [
+                'id' => $objectExt->object->id,
+                'height' => $objectExt->object->height,
+                'width' => $objectExt->object->width,
+                'length' => $objectExt->object->length,
+                'rotate' => $objectExt->positionType == TerritoryConcept::HORIZONTAL_POSITION ? TerritoryConcept::HORIZONTAL_POSITION : 90,
+                'link' => $objectExt->object->model_path,
+                'dotCenter' => [
+                    'x' => $objectExt->left + $objectExt->object->width / 2,
+                    'y' => $objectExt->top + $objectExt->object->height / 2,
+                ],
+            ];
+        }
+
+        return json_encode(
+            [
+                'result' => [
+                    'matrixCount' => [
+                        'width' => count($matrix[0]),
+                        'height' => count($matrix),
+                        'maxHeight' => $maxHeight,
+                    ],
+                    'matrix' => $matrix,
+                    'objects' => $resultObjList,
+                ],
+            ],
+        );
     }
 }
